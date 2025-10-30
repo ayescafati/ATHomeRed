88115f6f5eddd211277e4002d9b090b3de250a48
IgnacioPuertas <162916192+IgnacioPuertas@users.noreply.github.com>
2025-10-30 20:19:32 -0300

    base api

---
+++ app/api/__init__.py
@@ -0,0 +1,12 @@
+"""
+API Package - FastAPI routers, schemas y dependencies
+"""
+from . import dependencies
+from . import schemas
+from . import routers
+
+__all__ = [
+    "dependencies",
+    "schemas",
+    "routers",
+]
